81736ea9340a3888c1db55fa5b6c89f9f09f39c4
Felipe Souza <fesemanuel2@gmail.com>
2023-04-03 14:32:46 -0300

    fix: apiURL

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const apiURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3333'
-    : 'https://vitavi-backend.cloud.med.br'
+    : 'https://clinicacross-backend.cloud.med.br'
